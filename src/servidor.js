c203956ded0da37abf00ec852eb4f53593a45ce1
federico CV <fedesosa2002@hotmail.com>
2023-11-05 21:36:46 -0300

    feature jwk

--- src/servidor.js
+++ src/servidor.js
@@ -3,10 +3,14 @@ require('dotenv').config();
 const express = require('express');
 const usuarioRouter = require('./routes/usuarioRoutes.js');
 
+const autenticacionRouter = require('./routes/AutenticacionRoutes.js');
 const app = express();
 const PORT = 3000;
 
-app.use(usuarioRouter);
+app.use(usuarioRouter); 
+app.use(autenticacionRouter); 
+
+
 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto ${PORT}`);
